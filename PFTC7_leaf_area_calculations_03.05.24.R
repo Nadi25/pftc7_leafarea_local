e24310953561201366cfd9d896833b3e94625597
Nadi25 <nadine-arzt@gmx.de>
2024-05-11 22:26:56 +0200

    folders 5 and 6

--- PFTC7_leaf_area_calculations_03.05.24.R
+++ PFTC7_leaf_area_calculations_03.05.24.R
@@ -16,3 +16,27 @@ folder_path <- "raw_scans/5/"
 output_folder <- "output/output_leaf_area/5/"
 leaf_areas <- leaf_area_calculation(folder_path = folder_path, output_folder = output_folder)
 leaf_areas
+
+# Write the leaf areas to a CSV file
+write.csv(leaf_areas, file = "raw_data/EBImage/PFTC7_leaf_area_5.csv", row.names = FALSE)
+
+leaf_area_5 <- read.csv("raw_data/EBImage/PFTC7_leaf_area_5.csv")
+leaf_area_5
+
+
+# folder 6 ----------------------------------------------------------------
+
+folder_path <- "raw_scans/6/"
+output_folder <- "output/output_leaf_area/6/"
+leaf_areas <- leaf_area_calculation(folder_path = folder_path, output_folder = output_folder)
+leaf_areas
+
+# Write the leaf areas to a CSV file
+write.csv(leaf_areas, file = "raw_data/EBImage/PFTC7_leaf_area_6.csv", row.names = FALSE)
+
+leaf_area_6 <- read.csv("raw_data/EBImage/PFTC7_leaf_area_6.csv")
+leaf_area_6
+
+
+
+
