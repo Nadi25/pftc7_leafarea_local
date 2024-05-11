89ee01ae03f6e8be59961e5d7e2413dd9ec9e38d
Nadi25 <nadine-arzt@gmx.de>
2024-05-11 22:26:13 +0200

    leaf area in cm2, make argument for dpi and cropping

--- PFTC7_functions_image_prep_and_process_09.05.24.R
+++ PFTC7_functions_image_prep_and_process_09.05.24.R
@@ -7,7 +7,9 @@ library(purrr)
 
 # function to calculate leaf area from a folder of jpeg images ------------
 
-leaf_area_calculation <- function(folder_path, output_folder, method = "Otsu") {
+leaf_area_calculation <- function(folder_path, output_folder, method = "Otsu",
+                                  crop_top = 10, crop_bottom = 2400,
+                                  crop_left = 1750, crop_right = 3513) {
   # List JPEG files in the folder
   list.of.files <- dir(path = folder_path, pattern = "jpeg", full.names = TRUE)
 
@@ -20,10 +22,10 @@ leaf_area_calculation <- function(folder_path, output_folder, method = "Otsu") {
     img_dims <- dim(img)
 
     # Define the cropping dimensions
-    crop_top <- 10
-    crop_bottom <- min(2400, img_dims[1])  # Ensure the bottom crop does not exceed image height
-    crop_left <- 1750
-    crop_right <- min(3513, img_dims[2])  # Ensure the right crop does not exceed image width
+    crop_top <- crop_top
+    crop_bottom <- min(crop_bottom, img_dims[1])  # Ensure the bottom crop does not exceed image height
+    crop_left <- crop_left
+    crop_right <- min(crop_right, img_dims[2])  # Ensure the right crop does not exceed image width
 
     # Crop the image
     img_crop <- img[crop_top:crop_bottom, crop_left:crop_right, , drop = FALSE ]
@@ -43,17 +45,17 @@ leaf_area_calculation <- function(folder_path, output_folder, method = "Otsu") {
 # method can be chosen
 # exclude small particles
 
-process_image_leaf_area <- function(img, method = "Otsu", min_area_mm2 = 1, output_folder, img_file) {
+process_image_leaf_area <- function(img, method = "Otsu", dpi = 300, min_area_mm2 = 1, output_folder, img_file) {
 
   # Process the cropped image
 
-    # Convert the image to bluescale
+    # Convert the image to blue scale
     blue_img <- channel(img, "blue")
 
     # Convert to 8-bits (hopefully)
     data <- round(blue_img * 256)
 
-    # Set threshold using autothresh method
+    # Set threshold using auto thresh method
     threshold <- autothresholdr::auto_thresh(data, method = method) / 256
     threshold_img <- blue_img >= threshold
 
@@ -64,7 +66,7 @@ process_image_leaf_area <- function(img, method = "Otsu", min_area_mm2 = 1, outp
     leaf_areas <- computeFeatures.shape(segmented_img)[, "s.area"]
 
     # Convert leaf area to mm^2 (assuming known pixel-to-mm conversion factor)
-    dpi <- 300
+    dpi <- dpi
     leaf_areas_mm2 <- (leaf_areas / dpi^2) * 25.4^2
 
     # Filter out small particles
@@ -76,6 +78,9 @@ process_image_leaf_area <- function(img, method = "Otsu", min_area_mm2 = 1, outp
     # Calculate total leaf area
     total_leaf_area_mm2 <- sum(filtered_leaf_areas_mm2)
 
+    # convert to cm2 and round to 3 digits
+    total_leaf_area_cm2 <- round(total_leaf_area_mm2/100, 3)
+
     # Save the segmented image
     output_path <- file.path(output_folder, basename(img_file))
     writeImage(threshold_img, output_path) # save the image with original name
@@ -83,14 +88,9 @@ process_image_leaf_area <- function(img, method = "Otsu", min_area_mm2 = 1, outp
     # Add results to the data frame
     data.frame(Image = basename(img_file),
                num_patches = num_patches,
-               total_leaf_area_mm2 = total_leaf_area_mm2)
+               total_leaf_area_mm2 = total_leaf_area_mm2,
+               total_leaf_area_cm2 = total_leaf_area_cm2)
 
 }
-
-
-
-
-
-
 
 
