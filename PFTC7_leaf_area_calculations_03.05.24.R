0f46cfb874a170acc96155eef6668d62049b2914
Nadi25 <nadine-arzt@gmx.de>
2024-05-11 13:22:40 +0200

    source new functions

--- PFTC7_leaf_area_calculations_03.05.24.R
+++ PFTC7_leaf_area_calculations_03.05.24.R
@@ -2,13 +2,17 @@
 # PFTC7 leaf area
 # Get leaf area from different folders
 
-source("Leaf_area_EBI_function_02.05.24.R")
+source("PFTC7_functions_image_prep_and_process_09.05.24.R")
 
-# Now you can use the leaf_area_EBImage function as before
-folder_path <- "raw_scans/6/"
-output_folder <- "output/output_EBI/6/"
-leaf_areas <- leaf_area_EBImage(folder_path, output_folder)
-print(leaf_areas)
+folder_path <- "test/"
+output_folder <- "test/output_otsu_new/"
+leaf_areas <- leaf_area_calculation(folder_path = folder_path, output_folder = output_folder)
+leaf_areas
 
-leaf_areas_mm2_6 <- read.csv("output/output_EBI/6/leaf_areas_mm2.csv")
-leaf_areas_mm2_6
+
+# folder 5 ----------------------------------------------------------------
+
+folder_path <- "raw_scans/5/"
+output_folder <- "output/output_leaf_area/5/"
+leaf_areas <- leaf_area_calculation(folder_path = folder_path, output_folder = output_folder)
+leaf_areas
