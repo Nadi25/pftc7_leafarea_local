79978be1250bb912897d83d5398b54ee6943c52f
Nadi25 <nadine-arzt@gmx.de>
2024-05-12 22:53:55 +0200

    try with method "minimum" and compare output with method "otsu"

--- PFTC7_leaf_area_calculations_03.05.24.R
+++ PFTC7_leaf_area_calculations_03.05.24.R
@@ -23,6 +23,21 @@ write.csv(leaf_areas, file = "raw_data/EBImage/PFTC7_leaf_area_5.csv", row.names
 leaf_area_5 <- read.csv("raw_data/EBImage/PFTC7_leaf_area_5.csv")
 leaf_area_5
 
+# folder 5 minimum ----------------------------------------------------------------
+
+folder_path <- "raw_scans/5/"
+output_folder <- "output/output_leaf_area/5_minimum/"
+leaf_areas <- leaf_area_calculation(folder_path = folder_path, output_folder = output_folder, method = "Minimum")
+leaf_areas
+
+# Write the leaf areas to a CSV file
+write.csv(leaf_areas, file = "raw_data/EBImage/PFTC7_leaf_area_5_minimum.csv", row.names = FALSE)
+
+leaf_area_5_minimum <- read.csv("raw_data/EBImage/PFTC7_leaf_area_5_minimum.csv")
+leaf_area_5_minimum
+
+
+
 
 # folder 6 ----------------------------------------------------------------
 
@@ -36,6 +51,32 @@ write.csv(leaf_areas, file = "raw_data/EBImage/PFTC7_leaf_area_6.csv", row.names
 
 leaf_area_6 <- read.csv("raw_data/EBImage/PFTC7_leaf_area_6.csv")
 leaf_area_6
+
+# folder 6 minimum ----------------------------------------------------------------
+
+folder_path <- "raw_scans/6/"
+output_folder <- "output/output_leaf_area/6_minimum/"
+leaf_areas <- leaf_area_calculation(folder_path = folder_path, output_folder = output_folder, method = "Minimum")
+leaf_areas
+
+# Write the leaf areas to a CSV file
+write.csv(leaf_areas, file = "raw_data/EBImage/PFTC7_leaf_area_6_minimum.csv", row.names = FALSE)
+
+leaf_area_6_minimum <- read.csv("raw_data/EBImage/PFTC7_leaf_area_6_minimum.csv")
+leaf_area_6_minimum
+
+
+# compare the methods -----------------------------------------------------
+
+# Assuming you have data frames 'df_otsu' and 'df_minimum' containing the results from the Otsu and Minimum methods respectively.
+
+# Merge the data frames for both methods based on the image filename
+merged_df <- merge(leaf_area_6, leaf_area_6_minimum, by = "Image", suffixes = c("_otsu", "_minimum"))
+
+# Calculate the difference in leaf area between the two methods for each image
+merged_df$leaf_area_difference_cm2 <- merged_df$total_leaf_area_cm2_otsu - merged_df$total_leaf_area_cm2_minimum
+
+
 
 
 
